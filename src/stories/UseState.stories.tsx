8d17162aab984ac0625a9c29ef8c43ddbd980cbd
chrismelyan <chris.shelpuk@gmail.com>
2022-04-09 13:45:49 +0200

    useState features

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+
+export default {
+    title: 'useState demo'
+}
+// useState не может принимать функцию как инициализованное значение, но он может принять функцию,
+// которая при вызове отдасть инициаллизованное значение, которое запишет useState и при чем useState сработает по принципу useMemo.
+
+export const Example1 = () => {
+    console.log('counting')
+    // const [counter, setCounter] = useState<number>(0)
+    const [counter, setCounter] = useState<number>(() => {
+        console.log('generated Data')
+        return 3576295;
+    })
+
+    return <>
+        <button onClick={() => {setCounter(counter + 1)}}>+</button>
+        {counter}
+    </>
+}
+
+export const Example2 = () => {
+    console.log('counting')
+    const [counter, setCounter] = useState<number>(0)
+    // setCounter может принимать функци. Он не запоминает ее, но принимает и эта ыункция возвращает изменения со стейтом
+    const changer = (state: number) => state + 1
+
+    return <>
+        <button onClick={() => setCounter(changer)}>+</button>
+        {counter}
+    </>
+}
